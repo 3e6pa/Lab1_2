9466a8fe3760aef60e9e544f2a580a873663815c
3e6pa <viks1001viktor@gmail.com>
2015-12-11 06:23:29 +0200

    First task. Optimization norm :) + sum vectors + scalar

--- src/lab1_2/Lab1_2.java
+++ src/lab1_2/Lab1_2.java
@@ -31,12 +31,18 @@ public class Lab1_2 {
         vect2.addArray(vect1);
         vect2.showArray();
 
-        if (vect1.compare(vect2) == 0) {
-            System.out.println("vectors are equals");
-        } else if (vect1.compare(vect2) == 1) {
-            System.out.println("vector1 a lot than vector2");
-        } else if (vect1.compare(vect2) == 2) {
-            System.out.println("vector1 a less than vector2");
+        switch (vect1.compare(vect2)) {
+            case 0:
+                System.out.println("vectors are equals");
+                break;
+            case 1:
+                System.out.println("vector1 a lot than vector2");
+                break;
+            case 2:
+                System.out.println("vector1 a less than vector2");
+                break;
+            default:
+                break;
         }
         double d = vect1.getElement(1);
         System.out.println("1 = " + d);
